cfbdc3cfddcd175fb60f3138e0332f26c28fecc8
Nazareno <nazarenochialva@hotmail.com>
2025-08-30 19:28:25 -0300

    Cree Prefab Varianat de Eenmigo, y camara
    Agregue otro tipo de enemigo. un Orc.
    Y una nueva camara

---
+++ PV2NazarenoLChialva/Assets/Scripts/Jugador.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+    // Aquí defines tus variables y métodos, como el de ModificarVida
+    public void ModificarVida(float puntosDeDanio)
+    {
+        // Tu lógica para modificar la vida del jugador aquí
+    }
+}
